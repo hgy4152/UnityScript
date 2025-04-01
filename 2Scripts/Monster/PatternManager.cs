76b900011479b494170d0094561cafbab716413d
hgy4152 <hgy4152@gmail.com>
2025-04-01 21:23:41 +0900

    Rename
    Folder

--- 2Scripts/Monster/PatternManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class PatternManager : MonoBehaviour
-{
-    public abstract void Battle(Animator anim, MonsterData data);
-}
